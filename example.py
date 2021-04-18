8f4cc60c1091902434157a3180d45c6317d10dbe
Christian Köver - Draxl <77546092+ckoever@users.noreply.github.com>
2021-04-18 17:22:35 +0200

    Update example.py

--- example.py
+++ example.py
@@ -5,14 +5,14 @@ import time as MOD_TIME
 #Connect to Wifi
 GLOB_WLAN=MOD_NETWORK.WLAN(MOD_NETWORK.STA_IF)
 GLOB_WLAN.active(True)
-GLOB_WLAN.connect("YOURSSID", "YOURPASSWD")
+GLOB_WLAN.connect("SSID", "PASSWD")
 
 while not GLOB_WLAN.isconnected():
   pass
 
 #firebase example
 import ufirebase as firebase
-firebase.setURL("https://YOURDATABASE")
+firebase.setURL("https://DATABASE.firebaseio.com/")
 
 #Put Tag1
 firebase.put("testtag", "1234", bg=0)
@@ -22,12 +22,23 @@ firebase.put("lolval/testval", {"somenumbers": [1,2,3], "something": "lol"}, bg=
 
 #Get Tag1
 firebase.get("testtag", "var1", bg=0)
-print(firebase.var1)
+print("testtag: "+str(firebase.var1))
 
 #Get Tag2
-firebase.get("lolval", "lolwhat", bg=1)
-print("Im Downloading...")
-MOD_TIME.sleep(5) #Do something in this time
-print("lolval_1: "+str(firebase.lolwhat["testval"]["somenumbers"])+
+def callbackfunc():
+  print("\nlolval_1: "+str(firebase.lolwhat["testval"]["somenumbers"])+
   "\nlolval_2: "+str(firebase.lolwhat["testval"]["something"])+
   "\nlolall: "+str(firebase.lolwhat))
+
+firebase.get("lolval", "lolwhat", bg=1, cb=(callbackfunc, ()))
+print(end="Im getting lolval now")
+
+#Do something in this time
+while 1:
+  print(end=".")
+  MOD_TIME.sleep(.100)
+  try: 
+    firebase.lolwhat
+    break
+  except:
+    pass
